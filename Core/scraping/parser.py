939d673741f09a4a35e30a2775e7cd97d85d6e1c
Dang Tu Anh <58834452+tuanh610@users.noreply.github.com>
2020-03-01 18:46:13 +0800

    Add static files

---
+++ Core/scraping/parser.py
@@ -0,0 +1,4 @@
+from Core.scraping.HoangHaMobileScraper import HoangHaMobileScraper
+from Core.scraping.TheGioiDiDongScaper import TheGioiDiDongScraper
+
+parser = {"hoanghaMobile": HoangHaMobileScraper, 'thegioididong': TheGioiDiDongScraper}
